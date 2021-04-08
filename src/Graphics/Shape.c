803520ec747335bb9e2244ddbd940870d34884d5
Rafał Jopek <rafaljopek@hotmail.com>
2021-04-09 00:51:20 +0200

    Update

--- src/Graphics/Shape.c
+++ src/Graphics/Shape.c
@@ -338,16 +338,136 @@ HB_FUNC( SFSHAPE_GETTRANSFORM )
 }
 
 // sfTransform sfShape_getInverseTransform(const sfShape* shape);
+HB_FUNC( SFSHAPE_GETINVERSETRANSFORM )
+{
+   const sfShape* shape = hb_sfShape_param( 1 );
+
+   if( shape )
+   {
+      sfTransform sftransform = sfShape_getInverseTransform( shape );
+
+      PHB_ITEM info = hb_itemArrayNew( 9 );
+
+      hb_arraySetND( info, 1, ( float ) sftransform.matrix[ 0 ] );
+      hb_arraySetND( info, 2, ( float ) sftransform.matrix[ 1 ] );
+      hb_arraySetND( info, 3, ( float ) sftransform.matrix[ 2 ] );
+      hb_arraySetND( info, 4, ( float ) sftransform.matrix[ 3 ] );
+      hb_arraySetND( info, 5, ( float ) sftransform.matrix[ 4 ] );
+      hb_arraySetND( info, 6, ( float ) sftransform.matrix[ 5 ] );
+      hb_arraySetND( info, 7, ( float ) sftransform.matrix[ 6 ] );
+      hb_arraySetND( info, 8, ( float ) sftransform.matrix[ 7 ] );
+      hb_arraySetND( info, 9, ( float ) sftransform.matrix[ 8 ] );
+
+      hb_itemReturnRelease( info );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // void sfShape_setTexture(sfShape* shape, const sfTexture* texture, sfBool resetRect);
+HB_FUNC( SFSHAPE_SETTEXTURE )
+{
+   sfShape* shape = hb_sfShape_param( 1 );
+   const sfTexture* texture = hb_sfTexture_param( 2 );
+
+   if( shape )
+   {
+      sfShape_setTexture( shape, texture, hb_parl( 3 ) );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // void sfShape_setTextureRect(sfShape* shape, sfIntRect rect);
+HB_FUNC( SFSHAPE_SETTEXTURERECT )
+{
+   PHB_ITEM pItem;
+
+   sfShape* shape = hb_sfShape_param( 1 );
+
+   if( shape && ( pItem = hb_param( 2, HB_IT_ARRAY ) ) != NULL && hb_arrayLen( pItem ) == 4 )
+   {
+      sfIntRect rect;
+
+      rect.left   = hb_arrayGetNI( pItem, 1 );
+      rect.top    = hb_arrayGetNI( pItem, 2 );
+      rect.width  = hb_arrayGetNI( pItem, 3 );
+      rect.height = hb_arrayGetNI( pItem, 4 );
+
+      sfShape_setTextureRect( shape, rect );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // void sfShape_setFillColor(sfShape* shape, sfColor color);
+HB_FUNC( SFSHAPE_SETFILLCOLOR )
+{
+   PHB_ITEM pItem;
+
+   sfShape* shape = hb_sfShape_param( 1 );
+
+   if( shape && ( pItem = hb_param( 2, HB_IT_ARRAY ) ) != NULL && hb_arrayLen( pItem ) == 4 )
+   {
+      sfColor color;
+
+      color.r = ( unsigned char ) hb_arrayGetNI( pItem, 1 );
+      color.g = ( unsigned char ) hb_arrayGetNI( pItem, 2 );
+      color.b = ( unsigned char ) hb_arrayGetNI( pItem, 3 );
+      color.a = ( unsigned char ) hb_arrayGetNI( pItem, 4 );
+
+      sfShape_setFillColor( shape, color );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // void sfShape_setOutlineColor(sfShape* shape, sfColor color);
+HB_FUNC( SFSHAPE_SETOUTLINECOLOR )
+{
+   PHB_ITEM pItem;
+
+   sfShape* shape = hb_sfShape_param( 1 );
+
+   if( shape && ( pItem = hb_param( 2, HB_IT_ARRAY ) ) != NULL && hb_arrayLen( pItem ) == 4 )
+   {
+      sfColor color;
+
+      color.r = ( unsigned char ) hb_arrayGetNI( pItem, 1 );
+      color.g = ( unsigned char ) hb_arrayGetNI( pItem, 2 );
+      color.b = ( unsigned char ) hb_arrayGetNI( pItem, 3 );
+      color.a = ( unsigned char ) hb_arrayGetNI( pItem, 4 );
+
+      sfShape_setOutlineColor( shape, color );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // void sfShape_setOutlineThickness(sfShape* shape, float thickness);
+HB_FUNC( SFSHAPE_SETOUTLINETHICKNESS )
+{
+   sfShape* shape = hb_sfShape_param( 1 );
+
+   if( shape && hb_param( 2, HB_IT_NUMERIC ) != NULL )
+   {
+      sfShape_setOutlineThickness( shape, ( float ) hb_parnd( 2 ) );
+   }
+   else
+   {
+      hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
+   }
+}
 
 // const sfTexture* sfShape_getTexture(const sfShape* shape);
 
